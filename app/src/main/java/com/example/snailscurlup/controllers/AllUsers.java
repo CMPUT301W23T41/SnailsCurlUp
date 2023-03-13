f77d0ef54d86eaea0ed7332450df77f5bc02534c
Richard He <richardhe330@gmail.com>
2023-03-12 19:29:18 -0600

    add user to firebase

--- app/src/main/java/com/example/snailscurlup/controllers/AllUsers.java
+++ app/src/main/java/com/example/snailscurlup/controllers/AllUsers.java
@@ -2,9 +2,17 @@ package com.example.snailscurlup.controllers;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 
 import com.example.snailscurlup.model.User;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class AllUsers {
@@ -13,10 +21,14 @@ public class AllUsers {
     private static AllUsers instance;
     private final SharedPreferences sharedPreferences;
     private final Context context;
+    FirebaseFirestore db;
+
 
     // constructor
     private AllUsers(Context context) {
         this.context = context.getApplicationContext();
+        db = FirebaseFirestore.getInstance();
+
         users = SharedPreferencesUtils.getAllUsers(context);
         sharedPreferences = context.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
         String activeUsername = sharedPreferences.getString("activeUsername", null);
@@ -35,9 +47,36 @@ public class AllUsers {
 
     // Method to add a new user
     public void addUser(String username, String email, String phoneNumber, String profilePhotoPath, String device_id) {
+        //local database
         User newUser = new User(username, email, phoneNumber, profilePhotoPath, device_id);
         SharedPreferencesUtils.addUser(context, newUser);
         users.add(newUser);
+        //Firebase add user
+        CollectionReference collectionReference = db.collection("Users");
+        final String TAG = "Sample";
+        HashMap<String, String> data = new HashMap<>();
+        if (username.length() > 0) {  //if (userName.length() > 0 && email.length() > 0 && phoneNumber.length() > 0)
+            data.put("Email", email);
+            data.put("PhoneNumber", phoneNumber);
+            data.put("Total Score", "0");
+            data.put("Codes Scanned","0");
+            collectionReference
+                    .document(username)
+                    .set(data)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Log.d(TAG, "Data has been added successfully!");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                            Log.d(TAG, "Data not be added!" + e.toString());
+                        }
+                    });
+        }
     }
 
     // Method to remove an existing user
